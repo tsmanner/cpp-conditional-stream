aecad55f3fe4f6c21dc37b72c1bcd671775620d3
Tom Manner <tsmanner@us.ibm.com>
2021-05-11 22:28:58 -0400

    Added -std=c++11 flag to compile

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env sh
-nix run nixpkgs.clang_11 -c clang++ -g -Wall -Wextra -Werror -Iinclude main.cpp
+nix run nixpkgs.clang_11 -c clang++ -g -std=c++11 -Wall -Wextra -Werror -Iinclude main.cpp
